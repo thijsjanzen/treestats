1666ef83b8143bcac4fe0b314261c97361f01823
Thijs Janzen <thijsjanzen@gmail.com>
2021-10-22 12:33:26 +0200

    Update test-gamma.R

--- tests/testthat/test-gamma.R
+++ tests/testthat/test-gamma.R
@@ -2,7 +2,7 @@ context("gamma")
 
 test_that("usage", {
   set.seed(42)
-  focal_tree <- TreeSim::sim.bd.taxa(n = 100, numbsim = 1, lambda = 1, mu = 0)[[1]]
+  focal_tree <- TreeSim::sim.bd.taxa(n = 10, numbsim = 1, lambda = 1, mu = 0)[[1]]
 
   gammast <- treestats::gamma_statistic(focal_tree)
   gammast_check <- ape::gammaStat(focal_tree)
