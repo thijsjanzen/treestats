77d585dc3fb6e7a6fca00273ea3132310ab8144f
Thijs Janzen <thijsjanzen@gmail.com>
2021-12-16 21:42:00 +0100

    Update test-branching_times.R

--- tests/testthat/test-branching_times.R
+++ tests/testthat/test-branching_times.R
@@ -11,7 +11,7 @@ test_that("usage", {
 
   diff_brts <- abs(sort(a1) - sort(a2))
   mean_diff <- mean(diff_brts)
-  testthat::expect_lt(mean_diff, 1e-7)
+  testthat::expect_lt(mean_diff, 1e-6)
 
   # again, but with extinct lineages:
   set.seed(42)
@@ -20,9 +20,9 @@ test_that("usage", {
                                      lambda = 1, mu = 0.3)[[1]]
 
   a1 <- ape::branching.times(focal_tree)
-  a2 <- treestats::branching_times_treestats(focal_tree)
+  a2 <- treestats::branching_times(focal_tree)
 
   diff_brts <- abs(sort(a1) - sort(a2))
   mean_diff <- mean(diff_brts)
-  testthat::expect_lt(mean_diff, 1e-7)
+  testthat::expect_lt(mean_diff, 1e-6)
 })
