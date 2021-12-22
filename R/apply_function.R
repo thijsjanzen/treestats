dece820ee11fc064c55355447b6d88097d6434e7
Thijs Janzen <thijsjanzen@gmail.com>
2021-12-22 13:48:33 +0100

    remove commented code

--- R/apply_function.R
+++ R/apply_function.R
@@ -5,6 +5,5 @@ apply_function_phy <- function(input_phy, stat_function, ...) {
     stop("object \"phy\" is not of class \"phylo\"")
   }
 
-  # input_phy <- geiger::drop.extinct(input_phy)
   return(stat_function(input_phy, ...))
 }
