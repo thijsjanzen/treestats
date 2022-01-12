caf6848a18cfa1427a9b6d7c84570c67b524dfe4
Thijs Janzen <thijsjanzen@gmail.com>
2022-01-12 16:15:02 +0100

    update branching times

--- src/branching_times.h
+++ src/branching_times.h
@@ -13,27 +13,37 @@ std::vector< double > branching_times(const Rcpp::List& phy) {
   size_t Nnode = phy["Nnode"];
   size_t n = Nnode + 1;
 
-  std::vector<size_t> interns(Nnode);
-
   std::vector<double> edge_length = phy["edge.length"];
   Rcpp::NumericMatrix edge = phy["edge"];
 
-  size_t cnt = 0;
+  std::vector<double> xx(Nnode, 0.0);
+
   for (size_t i = 0; i < edge_length.size(); ++i) {
     if (edge(i, 1) > n) {
-      interns[cnt] = i;
-      cnt++;
+      auto target_index = edge(i, 1) - n - 1; // -1 because of R to C++ indexing
+      auto source_index = edge(i, 0) - n - 1;
+
+      if (target_index < 0 || target_index >= xx.size()) { // range checks
+        std::cerr << target_index << "\n";
+        throw std::out_of_range("target_index out of bounds");
+      }
+
+      if (source_index < 0 || source_index >= xx.size()) {
+        std::cerr << source_index << "\n";
+        throw std::out_of_range("source_index out of bounds");
+      }
+
+      xx[ target_index  ] = xx[ source_index] + edge_length[i];
     }
   }
 
-  std::vector<double> xx(Nnode);
-
-  for (const auto& i : interns) {
-    xx[ edge(i, 1) - n - 1 ] = xx[edge(i, 0) - n - 1] + edge_length[i];
+  auto edge_index = edge(edge_length.size() - 1, 0) - n - 1;
+  if (edge_index < 0 || edge_index >= xx.size()) {
+    std::cerr << edge_index << "\n";
+    throw std::out_of_range("edge_index out of bounds");
   }
 
-  int N = edge_length.size() - 1;
-  double depth = xx[edge(N, 0) - n - 1] +  edge_length[N];
+  double depth = xx[edge_index] +  edge_length[edge_length.size() - 1];
   for (auto& i : xx) {
     i = depth - i;
   }
