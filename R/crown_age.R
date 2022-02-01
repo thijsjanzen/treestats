e574e00bc0e7309da73a11c72e6ef76713d8b5e4
Thijs Janzen <thijsjanzen@gmail.com>
2022-02-01 14:39:49 +0100

    improve crown age

--- R/crown_age.R
+++ R/crown_age.R
@@ -1,7 +1,18 @@
-#' Calculates the crown age of a tree.
+#' Calculates the crown age of a tree using C++.
+#' In a reconstructed tree, obtaining the crown age is fairly straightforward,
+#' and the function beautier::get_crown_age does a great job at it.
+#' However, in a non-ultrametric tree, that function no longer works.
+#' Alternatively, taking the maximum value of adephylo::distRoot will also yield
+#' the crown age, but will typically perform many superfluous calculations and
+#' thus be slow.
 #' @param phy phylo object
 #' @return crown age
 #' @export
 crown_age <- function(phy) {
+
+  if (is.null(phy$root.edge)) {
+    phy$root.edge <- 0
+  }
+
   return(apply_function_phy(phy, calc_crown_age_cpp))
 }
