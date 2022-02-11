53fff8f106ae0996a43436b0eb863a226859f1a2
Thijs Janzen <thijsjanzen@gmail.com>
2022-02-11 18:25:20 +0100

    add all stats

---
+++ R/calc_all_stats.R
@@ -0,0 +1,51 @@
+#' function to apply all available tree statistics to a single tree
+#' @param phylo phylo object
+#' @return list with statistics
+#' @export
+#' @description this function applies all available tree statistics to a
+#' single tree, being:
+#' \itemize{
+#'   \item{gamma}
+#'   \item{sackin}
+#'   \item{colless}
+#'   \item{beta}
+#'   \item{blum}
+#'   \item{crown age}
+#'   \item{tree height}
+#'   \item{pigot's rho}
+#'   \item{mean branch length}
+#'   \item{number of lineages}
+#'   \item{laplacian spectrum}
+#'   \item{nLTT with empty tree}
+#'   \item{phylogenetic diversity}
+#'   \item{avgLadder index}
+#'   \item{cherries}
+#'   \item{ILnumber}
+#'   \item{pitchforks}
+#'   \item{stairs}
+#' }
+calc_all_stats <- function(phylo) {
+
+  stats <- list()
+
+  stats$gamma              <- treestats::gamma_statistic(phylo)
+  stats$sackin             <- treestats::sackin(phylo)
+  stats$colless            <- treestats::colless(phylo)
+  stats$beta               <- treestats::beta_statistic(phylo)
+  stats$blum               <- treestats::blum(phylo)
+  stats$crown_age          <- treestats::crown_age(phylo)
+  stats$tree_height        <- treestats::tree_height(phylo)
+  stats$pigot_rho          <- treestats::pigot_rho(phylo)
+  stats$mean_branch_length <- treestats::mean_branch_length(phylo)
+  stats$number_of_lineages <- treestats::number_of_lineages(phylo)
+  stats$laplac_spectrum    <- treestats::calc_lapl_spectrum(phylo)
+  stats$nltt_base          <- treestats::nLTT_base(phylo)
+  stats$phylogenetic_div   <- treestats::phylogenetic_diversity(phylo)
+  stats$avgLadder          <- treestats::avgLadder(phylo)
+  stats$cherries           <- treestats::cherries(phylo)
+  stats$ILnumber           <- treestats::ILnumber(phylo)
+  stats$pitchforks         <- treestats::pitchforks(phylo)
+  stats$stairs             <- treestats::stairs(phylo)
+
+  return(stats)
+}
---
+++ tests/testthat/test-allstats.R
@@ -0,0 +1,11 @@
+context("all_Stats")
+
+test_that("usage", {
+  set.seed(42)
+  focal_tree <- TreeSim::sim.bd.taxa(n = 100,
+                                     numbsim = 1,
+                                     lambda = 1, mu = 0)[[1]]
+
+  all_stats <- treestats::calc_all_stats(focal_tree)
+  testthat::expect_equal(length(all_stats), 18)
+})
