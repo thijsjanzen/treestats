14f47cdbe7f9c98bc6eac4a1007dbb519abd571b
Thijs Janzen <thijsjanzen@gmail.com>
2022-04-06 14:48:05 +0200

    Update test-blum.R

--- tests/testthat/test-blum.R
+++ tests/testthat/test-blum.R
@@ -2,19 +2,12 @@ context("blum")
 
 test_that("usage", {
   set.seed(42)
-  castor_version <- packageVersion("castor")
-  if (castor_version <= "1.7.2") {
-    testthat::skip("old castor has wrong blum calculation")
-    testthat::skip_on_cran("old castor has wrong blum calculation")
-    testthat::skip_on_ci("old castor has wrong blum calculation")
-
-  }
   focal_tree <- TreeSim::sim.bd.taxa(n = 100,
                                      numbsim = 1,
                                      lambda = 1, mu = 0)[[1]]
 
   blum1 <- treestats::blum(focal_tree)
-  blum_check <- castor::tree_imbalance(focal_tree, type = "Blum")
+  blum_check <- treebalance::sShapeI(focal_tree, logbase = exp(1))
   testthat::expect_equal(blum1, blum_check)
   ltab <- treestats::phylo_to_l(focal_tree)
   blum2 <- treestats::blum(ltab)
