ca8465336d65cdc10c534eaaac56da0857bf71ee
EvoLandEco <tianjianqin@outlook.com>
2022-05-06 16:35:38 +0200

    add std::abs to ensure both branch lengths are non-negative

--- inst/include/L2newick_ed.h
+++ inst/include/L2newick_ed.h
@@ -49,9 +49,9 @@ std::string ltable_to_newick_ed(const std::vector< std::array< double, 4>>& ltab
     int parent    = static_cast<int>(L[j][1]);
     int parentj   = index_of_parent(L, parent);
     if (parentj != -1) {
-      double bl = L[parentj][3] - L[j][0];
+      double bl = std::abs(L[parentj][3] - L[j][0]);
       std::string spec1 = linlist_4[parentj] + ":" + d_to_s(bl);
-      double bl2 = L[j][3] - L[j][0];
+      double bl2 = std::abs(L[j][3] - L[j][0]);
       std::string spec2 = linlist_4[j] + ":" + d_to_s(bl2);
       linlist_4[parentj] = "(" + spec1 + "," + spec2 + ")";
       L[parentj][3] = L[j][0];
