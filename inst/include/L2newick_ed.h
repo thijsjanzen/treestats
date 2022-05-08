63841cde230190d17a2b7709a763f242eee66082
EvoLandEco <tianjianqin@outlook.com>
2022-05-08 15:20:24 +0200

    try to avoid round error issue

--- inst/include/L2newick_ed.h
+++ inst/include/L2newick_ed.h
@@ -20,7 +20,7 @@ std::string ltable_to_newick_ed(const std::vector< std::array< double, 4>>& ltab
   std::vector< std::array< double, 4>> new_L;
 
   for (auto& i : L) {
-    bool is_extant = i[3] == -1;
+    bool is_extant = i[3] < 0;
     if (is_extant) {
       i[3] = age;
       if (drop_extinct == true) {
@@ -48,7 +48,7 @@ std::string ltable_to_newick_ed(const std::vector< std::array< double, 4>>& ltab
     auto j = which_max_index(L);
     int parent    = static_cast<int>(L[j][1]);
     int parentj   = index_of_parent(L, parent);
-    if (parentj != -1) {
+    if (parentj > -1) {
       double bl = std::abs(L[parentj][3] - L[j][0]);
       std::string spec1 = linlist_4[parentj] + ":" + d_to_s(bl);
       double bl2 = std::abs(L[j][3] - L[j][0]);
