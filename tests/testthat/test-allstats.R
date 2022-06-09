ad6f970ca5b9b335814ef2a452ae4bc521fc051e
Thijs Janzen <thijsjanzen@gmail.com>
2022-06-09 12:06:06 +0200

    Update test-allstats.R

--- tests/testthat/test-allstats.R
+++ tests/testthat/test-allstats.R
@@ -10,4 +10,20 @@ test_that("usage", {
    all_stats <- treestats::calc_all_stats(focal_tree)
   )
   testthat::expect_equal(length(all_stats), 41)
+
+
+  focal_tree <- ape::rphylo(n = 23171, birth = 1, death = 0)
+  testthat::expect_invisible(
+    all_stats <- treestats::calc_all_stats(focal_tree)
+  )
+  testthat::expect_true(is.na(all_stats$laplac_spectrum_a))
+  testthat::expect_true(is.na(all_stats$laplac_spectrum_p))
+  testthat::expect_true(is.na(all_stats$laplac_spectrum_e))
+  testthat::expect_true(is.na(all_stats$laplac_spectrum_g))
+
+  testthat::expect_true(is.na(all_stats$mntd))
+  testthat::expect_true(is.na(all_stats$mpd))
+  testthat::expect_true(is.na(all_stats$psv))
+  testthat::expect_true(is.na(all_stats$vpd))
+  testthat::expect_true(is.na(all_stats$j_stat))
 })
