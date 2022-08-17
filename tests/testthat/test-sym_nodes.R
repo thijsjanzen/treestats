e75acdebc3855b501517defabd438375eb771dfc
Thijs Janzen <thijsjanzen@gmail.com>
2022-08-17 14:06:01 +0200

    improve testing

--- tests/testthat/test-sym_nodes.R
+++ tests/testthat/test-sym_nodes.R
@@ -23,3 +23,27 @@ test_that("usage", {
   testthat::expect_equal(treestats::sym_nodes(focal_tree),
                          treestats::sym_nodes(ltab))
 })
+
+
+test_that("normalization", {
+  set.seed(42)
+  focal_tree <- ape::rphylo(n = 30, birth = 1, death = 0)
+
+  c1 <- treestats::sym_nodes(focal_tree)
+  c2 <- treestats::sym_nodes(focal_tree, normalization = "tips")
+  testthat::expect_lt(c2, c1)
+
+  stats1 <- c()
+  stats2 <- c()
+  for (n in seq(100, 200, by = 10)) {
+    focal_tree <- ape::rphylo(n = n, birth = 1, death = 0)
+    stats1 <- c(stats1, treestats::sym_nodes(focal_tree))
+    stats2 <- c(stats2, treestats::sym_nodes(focal_tree, normalization = "tips"))
+  }
+
+  a1 <- cor(stats1, seq(100, 200, by = 10))
+  a2 <- cor(stats2, seq(100, 200, by = 10))
+
+  testthat::expect_lt(a2, a1)
+  testthat::expect_lt(a2, 0.2)
+})
