10bfeac51f297664eb1cf2ad1a45febab16a455d
Thijs Janzen <thijsjanzen@gmail.com>
2023-04-07 12:51:51 +0200

    improve linting

--- inst/include/ILnumber.h
+++ inst/include/ILnumber.h
@@ -1,9 +1,20 @@
-#ifndef IL_H
-#define IL_H
+// Copyright 2022 - 2023 Thijs Janzen
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//
+#pragma once
+#include <vector>
 
 size_t calc_IL(const std::vector< long >& tree_edge) {
-
-  int root_no = 2 + tree_edge.size() * 0.25;   //static_cast<int>(tree_edge.front());
+  int root_no = 2 + tree_edge.size() * 0.25;
   std::vector< int > nodes(tree_edge.size() / 2, 0);
 
   for (size_t i = 0; i < tree_edge.size(); i += 2 ) {
@@ -19,6 +30,3 @@ size_t calc_IL(const std::vector< long >& tree_edge) {
   }
   return(num_IL);
 }
-
-
-#endif
