c3134eeae367d23916c0157ff264eec645376cad
Thijs Janzen <thijsjanzen@gmail.com>
2023-05-08 11:35:08 +0200

    Update mpd.h

--- inst/include/mpd.h
+++ inst/include/mpd.h
@@ -15,30 +15,33 @@
 namespace mpd_tree {
 
 
-struct node {
-  node* daughter1 = nullptr;
-  node* daughter2 = nullptr;
+struct mpd_node {
+  mpd_node* daughterL = nullptr;
+  mpd_node* daughterR = nullptr;
   size_t L;
   size_t R;
   double bl_R;
   double bl_L;
 
-  node() {
-    daughter1 = nullptr;
-    daughter2 = nullptr;
-    L = 0;
-    R = 0;
+  mpd_node() {
+    daughterL = nullptr;
+    daughterR = nullptr;
+    L = 1;
+    R = 1;
     bl_R = -1.0;
     bl_L = -1.0;
   }
 
   size_t update_num_tips() {
-    if (daughter1 && !daughter2) {
-      L = daughter1->update_num_tips();
+    if (daughterL && !daughterR) {
+      L = daughterL->update_num_tips();
     }
-    if (daughter1 && daughter2) {
-      L =  daughter1->update_num_tips();
-      R =  daughter2->update_num_tips();
+    if (daughterR && !daughterL) {
+      R = daughterR->update_num_tips();
+    }
+    if (daughterL && daughterR) {
+      L =  daughterL->update_num_tips();
+      R =  daughterR->update_num_tips();
     }
 
     return L + R;
@@ -59,18 +62,16 @@ class phylo_tree {
       int d1_index = static_cast<int>(tree_edge[i + 1]) - root_no;
       int el_index = i / 2;
 
-      if (d1_index < 0) {
-        tree[index].R == 0 ? tree[index].R = 1 : tree[index].L = 1;
-      } else {
-        !tree[index].daughter1 ?
-         tree[index].daughter1 = &tree[d1_index] :
-         tree[index].daughter2 = &tree[d1_index];
+      if (d1_index >= 0) {
+        // we are dealing with an internal node
+        tree[index].bl_L < 0 ?  // was the left daughter already set before?
+          tree[index].daughterL = &tree[d1_index] :
+          tree[index].daughterR = &tree[d1_index];
       }
 
-      tree[index].bl_R < 0 ?
-        tree[index].bl_R = edge_length[el_index] :
-        tree[index].bl_L = edge_length[el_index];
-
+      tree[index].bl_L < 0 ?
+        tree[index].bl_L = edge_length[el_index] :
+        tree[index].bl_R = edge_length[el_index];
     }
 
     tree[0].update_num_tips();
@@ -85,51 +86,18 @@ class phylo_tree {
       auto L_bl = i.bl_L;
       auto R_bl = i.bl_R;
 
-      std::cerr << l << " " << L_bl << "\n";
-      std::cerr << r << " " << R_bl << "\n";
-
       mpd += L_bl * (l * (N - l));
       mpd += R_bl * (r * (N - r));
     }
-
-    // std::cerr << N << " " << mpd << "\n";
 
     mpd *= 2.0 / (N * (N - 1));
     return(mpd);
   }
 
  private:
-  std::vector< node > tree;
+  std::vector< mpd_node > tree;
   int tree_size = 0;
 };
 
 
 }  // namespace mpd_tree
-
-
-
-/*
- struct mpd_tree {
- mpd_tree(const std::vector< int >& tree_edge,
- const std::vector<double>& edge_length) : branch_length(edge_length){
- // we need to construct a bl, descendants matrix
- int root_no = 2 + static_cast<int>(0.25 * tree_edge.size());
- num_descendants = std::vector<int>(branch_length.size());
- for (int i = 1; i < tree_edge.size(); ++i) {
- if (tree_edge[i] < root_no) {
- num_descendants[i / 2] = 1;
- }
- }
- for (int i = 0; i < tree_edge.size(); i += 2) {
- int parent = tree_edge[i];
- int offspring = tree_edge[i + 1];
- num_descendants[parent] += num_descendants[offspring];
- }
-
- }
-
- private:
- const std::vector<double> branch_length;
- std::vector<int> num_descendants;
- };
- */
