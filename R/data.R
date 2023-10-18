dcbe321675d08ad8086c7526a33853e0e2e953be
Thijs Janzen <thijsjanzen@gmail.com>
2023-10-18 14:28:43 +0200

    add data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' @name sim_data
+#' @title simulation results
+#' @description Summary statistics for 1000 randomly sampled trees from all...
+#' @format list with two entries: cor containing the correlation calculated on
+#' the full dataset and data, containing the data.
+"sim_data"
+
+#' @name emp_data
+#' @title empirical results
+#' @description Summary statistics for empirical tree dataset
+#' @format list with two entries: cor containing the correlation calculated on
+#' the full dataset and data, containing the data.
+"sim_data"
