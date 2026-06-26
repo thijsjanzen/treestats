a10bb969e532cd20dcca93ef1101d26dd6e7ef70
Thijs Janzen <thijsjanzen@gmail.com>
2026-06-26 21:33:28 +0200

    fix test

--- tests/testthat/test-blum.R
+++ tests/testthat/test-blum.R
@@ -19,7 +19,7 @@ test_that("usage", {
   focal_tree <- ape::rphylo(n = 4, birth = 1, death = 0)
 
   blum1 <- treestats::blum(focal_tree)
-  blum_check <- log(2 - 1) + log(3 - 1) + log(4 - 1)
+  blum_check <- log2(2 - 1) + log2(3 - 1) + log2(4 - 1)
   testthat::expect_equal(blum1, blum_check)
   ltab <- treestats::phylo_to_l(focal_tree)
   blum2 <- treestats::blum(ltab)
