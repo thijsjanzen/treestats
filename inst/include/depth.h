744cb8681f1377e87aff0ebcb28520cc2dbcbf03
Thijs Janzen <thijsjanzen@gmail.com>
2026-07-02 21:24:40 +0200

    fix b2 statistic

--- inst/include/depth.h
+++ inst/include/depth.h
@@ -29,11 +29,13 @@ struct node_binary {
   int depth;
   int dist_to_tips;     // for b1 statistic
   int num_extant_tips;  // for sackin related statistics
+  double p;             // for b2 statistic
 
   node_binary() {
     depth = 0;
     dist_to_tips = 0;
     num_extant_tips = 0;
+    p = 0.0;
     daughter1 = nullptr;
     daughter2 = nullptr;
   }
@@ -83,6 +85,21 @@ struct node_binary {
     return num_extant_tips;
   }
 
+  void set_p(double parent_value) {
+    p = parent_value;
+    if (!daughter1 && !daughter2) return;
+
+    double num_daughters = 0;
+    if (daughter1) num_daughters++;
+    if (daughter2) num_daughters++;
+
+    double added_p = 1.0 / num_daughters;
+    if (daughter1) daughter1->set_p(p * added_p);
+    if (daughter2) daughter2->set_p(p * added_p);
+    
+    return;
+  }
+
 
   bool is_double_cherry() {
     if (num_extant_tips != 4) return false;
@@ -119,11 +136,13 @@ struct node_poly {
   int depth;
   int dist_to_tips;
   int num_extant_tips;
+  double p;
 
   node_poly() {
     depth = 0;
     dist_to_tips = 0;
     num_extant_tips = 0;
+    p = 0.0;
   }
 
   void add_daughter(node_poly* d) {
@@ -203,6 +222,16 @@ struct node_poly {
     if (answ < 0.5) answ = 1.0 - answ;
     return answ;
   }
+
+  void set_p(double parent_value) {
+    p = parent_value;
+    if (daughters.empty()) return;
+
+    double added_p = 1.0 / daughters.size();
+    for (auto& i : daughters) {
+      i->set_p(added_p * p);
+    }
+  }
 };
 
 struct tree_base {
@@ -222,6 +251,7 @@ struct tree_base {
   virtual size_t count_double_cherries() = 0;
   virtual size_t count_four_prong() = 0;
   virtual double root_imbalance() = 0;
+  virtual double calc_b2() = 0;
 };
 
 enum used_for {depth, b1, sackin};
@@ -399,6 +429,18 @@ class depth_tree : public tree_base {
   double root_imbalance() override {
     // we have previously verified the root is binary
     return tree[root_no].calc_root_imbalance();
+  }
+
+  double calc_b2() override {
+    tree[root_no].set_p(1.0);
+
+    // now we have to iterate over the tree:
+    double s = 0.0;
+    for (size_t i = 0; i < root_no; ++i) {
+      double p = tree[i].p;
+      if (p > 0) s += p * log2(p);
+    }
+    return -s;
   }
 };
 
